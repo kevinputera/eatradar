0c4b605a8d6128e7602c91fc7ce2b311b1e1579c
Kevin Putera <kevintiosanna@gmail.com>
2019-06-02 02:42:34 +0800

    add db scripts

---
+++ server/scripts/seed.js
@@ -0,0 +1,106 @@
+const { Client } = require("pg");
+const faker = require("faker");
+
+const client = new Client();
+
+(async () => {
+  await client.connect();
+
+  let street_pk = [];
+  let cuisine_pk = [];
+  let restaurant_pk = [];
+
+  // populate street table
+  await (async () => {
+    for (let i = 0; i < 100; i++) {
+      const name = faker.address.streetAddress(); 
+      const query = {
+        text: 'INSERT INTO street(name) VALUES ($1) RETURNING id',
+        values: [name]
+      };
+
+      let id;
+      try {
+        id = (await client.query(query)).rows[0].id;
+      } catch (e) {
+        console.log(`seed.js: error in street insert query: ${e}`);
+      }
+      street_pk.push(id);
+    } 
+  })();
+
+  // populate cuisine table
+  await (async () => {
+    for (let i = 0; i < 20; i++) {
+      const name = faker.random.word();
+      const query = {
+        text: 'INSERT INTO cuisine(name) VALUES ($1) RETURNING id',
+        values: [name]
+      }
+
+      let id;
+      try {
+        id = (await client.query(query)).rows[0].id;
+      } catch (e) {
+        console.log(`seed.js: error in cuisine insert query: ${e}`);
+      }
+      cuisine_pk.push(id);
+    }
+  })();
+
+  // populate restaurant table
+  await (async () => {
+    for (let i = 0; i < 500; i++) {
+      const name = faker.name.firstName() + "'s" + faker.company.catchPhrase();
+      const lon = faker.address.longitude();
+      const lat = faker.address.latitude();
+      const post = faker.address.zipCode();
+      const street_id = faker.random.number(street_pk.length - 1) + 1;
+      const query = {
+        text: 'INSERT INTO restaurant(name, location, postcode, street_id)' + 
+            'VALUES ($1, $2, $3, $4) RETURNING id',
+        values: [
+            name,
+            `POINT(${lon} ${lat})`,
+            post,
+            street_id
+        ]
+      }
+
+      let id;
+      try {
+        id = (await client.query(query)).rows[0].id;
+      } catch (e) {
+        console.log(`seed.js: error in restaurant insert query: ${e}`);
+      }
+      restaurant_pk.push(id);
+    }
+  })();
+
+  // populate restaurant cuisine join table
+  await (async () => {
+    for (let i = 0; i < 1000; i++) {
+      const restaurant_id = faker.random.number(restaurant_pk.length - 1) + 1;
+      const cuisine_id = faker.random.number(cuisine_pk.length - 1) + 1;
+      const query = {
+        text: 'INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id) VALUES ($1, $2)',
+        values: [
+            restaurant_id,
+            cuisine_id
+        ]
+      }
+
+      try {
+        await client.query(query);
+      } catch (e) {
+        console.log(`seed.js: error in restaurant_cuisine insert query: ${e}`);
+      }
+    }
+  })();
+
+  await client.end();
+
+})().then(() => {
+  console.log("seeding complete!")
+  process.exit(0);
+});
---
+++ server/scripts/tables.js
@@ -0,0 +1,63 @@
+const { Client } = require("pg");
+
+(async () => {
+  const client = new Client();
+  await client.connect();
+
+  const dropQuery = 
+      `DROP TABLE IF EXISTS restaurant_cuisine;
+       DROP TABLE IF EXISTS restaurant;
+       DROP TABLE IF EXISTS cuisine;
+       DROP TABLE IF EXISTS street;`;
+
+  const createQuery = 
+      `CREATE TABLE street (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(200) UNIQUE NOT NULL
+      );
+
+      CREATE TABLE cuisine (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) UNIQUE NOT NULL
+      );
+
+      CREATE TABLE restaurant (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(200) NOT NULL,
+        location GEOGRAPHY NOT NULL,
+        postcode VARCHAR(20),
+        street_id INTEGER NOT NULL,
+        FOREIGN KEY (street_id) REFERENCES street(id)
+          ON UPDATE CASCADE ON DELETE CASCADE
+      );
+      CREATE INDEX location_geog_idx ON restaurant USING GIST (location);
+
+      CREATE TABLE restaurant_cuisine (
+        id SERIAL PRIMARY KEY,
+        restaurant_id INTEGER NOT NULL,
+        cuisine_id INTEGER NOT NULL,
+        FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
+          ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (cuisine_id) REFERENCES cuisine(id)
+          ON UPDATE CASCADE ON DELETE CASCADE
+      );`;
+  
+  try {
+    await client.query(dropQuery);
+  } catch (e) {
+    console.log(`tables.js: error during deletion: ${e}`);
+    await client.end();
+  }
+  
+  try {
+    await client.query(createQuery);
+  } catch (e) {
+    console.log(`tables.js: error during creation: ${e}`);
+  } finally {
+    await client.end();
+  }
+
+})().then(() => {
+  console.log("tables.js: tables created!");
+  process.exit(0);
+});
