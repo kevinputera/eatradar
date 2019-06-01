9de63711fb8ab892251e84bbdd7f56d82caa8332
JustinChia1997 <justinchiajj@gmail.com>
2019-06-02 00:03:17 +0800

    Basic Server with a dash of routing

---
+++ server/Restaurants.js
@@ -0,0 +1,25 @@
+//Sample POJO to test out routing functionalities for restaurants
+const restaurants=[
+    {
+        id: 1,
+        name: 'WaaaaaaaCow',
+        email: 'wahcow@gmail.com',
+        status: 'active'
+    },
+ 
+    {
+        id: 2,
+        name: 'FineFoods',
+        email: 'FindFoods@gmail.com',
+        status: 'inactive'
+    },
+
+    {
+        id: 3,
+        name: 'Food Clique',
+        email: 'foodclick@gmail.com',
+        status: 'inactive'
+    }        
+];
+
+module.exports = restaurants;
---
+++ server/Reviews.js
@@ -0,0 +1,25 @@
+//Sample POJO to test out routing functionalities for reviewS
+const reviews=[
+    {
+        id: 1,
+        name: 'WaaaaaaaCow',
+        email: 'wahcow@gmail.com',
+        status: 'active'
+    },
+ 
+    {
+        id: 2,
+        name: 'FineFoods',
+        email: 'FindFoods@gmail.com',
+        status: 'inactive'
+    },
+
+    {
+        id: 3,
+        name: 'Food Clique',
+        email: 'foodclick@gmail.com',
+        status: 'inactive'
+    }        
+];
+
+module.exports = reviews;
