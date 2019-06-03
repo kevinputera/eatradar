6786bf4e5ba103e00ee6b9be22ce578646eed67f
Kevin Putera <kevintiosanna@gmail.com>
2019-06-03 21:51:51 +0800

    update restaurants route

---
+++ server/routes/restaurants.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const restaurantService = require('../service/restaurantService');
+const router = express.Router();
+
+// Get all Restaurants
+router.get('/', async (req,res) => {
+  const longitude = req.query.lon;
+  const latitude = req.query.lat;
+  let page = req.query.p;
+  let pageSize = req.query.ps;
+
+  if (longitude === null || latitude === null) {
+    res.status(400).send(
+        `Both longitude and latitude must be appended 
+         to the URI as query parameters`);
+    return;
+  }
+
+  if (page <= 0) {
+    res.status(400).send(
+        'Page query parameter[p] must be greater than zero');
+    return;
+  }
+
+  if (pageSize < 10) {
+    res.status(400).send(
+        'Page size must be greater than 10');
+    return;
+  }
+
+  try {
+    const result = await restaurantService.getRestaurants(
+        longitude, latitude, page, pageSize);
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// Get Single Restaurant based on id
+router.get('/:id', (req, res) => {
+  // res.json(restaurants.filter(restaurant => 
+      // restaurant.id === parseInt(req.params.id)));
+});
+
+module.exports =  router;
---
+++ server/routes/reviews.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Get all reviews
+router.get('/', (req,res) =>{
+  // TODO add in how we want to access api from elsewhere
+  // res.json(reviews);
+});
+
+// Get Single Review based on id
+router.get('/:id', (req, res) =>{
+  // res.json(reviews.filter(review => review.id === parseInt(req.params.id)));
+});
+
+module.exports =  router;
