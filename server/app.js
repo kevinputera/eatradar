2ed753f6bb04c48558b37a7f78a42fc9be085900
Kevin Putera <kevintiosanna@gmail.com>
2019-06-09 14:08:53 +0800

    add new routes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
-const restaurantRoute = require('./routes/restaurants');
-const reviewRoute = require('./routes/reviews');
+const restaurantRoute = require('./routes/restaurantRoute');
+const detailRoute = require('./routes/detailRoute');
+const reviewRoute = require('./routes/reviewsRoute');
+const blogPostRoute = require('./routes/blogPostRoute');
 
 const app = express();
-const PORT = process.env.EXPRESS_PORT || 5000;
 
-// Restaurant API routes
+// Use routes
 app.use('/restaurants', restaurantRoute);
+app.use('/details', detailRoute);
 app.use('/reviews', reviewRoute);
+app.use('/blogposts', blogPostRoute);
 
+const PORT = process.env.EXPRESS_PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server/trial.js
+++
@@ -1,6 +0,0 @@
-const restaurantService = require("./service/restaurantService");
-
-restaurantService.getRestaurants(0, 0, 1, 10)
-    .then(console.log)
-    .catch(console.log)
-    .then(() => process.exit(0));
