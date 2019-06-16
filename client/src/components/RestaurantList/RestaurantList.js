c9b147674eae31f7931312a05c0cc4350ee1402a
Kevin Putera <kevintiosanna@gmail.com>
2019-06-16 22:13:35 +0800

    add search onchange functionality

--- client/src/components/RestaurantList/RestaurantList.js
+++ client/src/components/RestaurantList/RestaurantList.js
@@ -1,37 +1,85 @@
 import React from 'react';
+import _ from 'lodash';
+import http from '../../utils/http';
 import './RestaurantList.css';
 
 class RestaurantList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude: '',
-      longitude: '',
+      latitude: 1.305004,
+      longitude: 103.772474,
+      page: 1,
+      pageSize: 10,
       query: '',
+      items: [],
     };
+    this.debouncedGetRestaurants = _.debounce(this.getRestaurants, 300);
   }
 
   async componentDidMount() {
+    const params = {
+      lat: this.state.latitude,
+      lng: this.state.longitude,
+      page: this.state.page,
+      pageSize: this.state.pageSize,
+    };
+    await this.getRestaurants(params);
   }
 
-  handleQueryInputChange = e => {
+  getRestaurants = async params => {
+    const res = await http.get('/restaurants', params);
+    const json = await res.json();
     this.setState({
-      query: e.target.value
+      items: json,
     });
   }
 
+  handleQueryInputChange = async e => {
+    this.setState({
+      query: e.target.value,
+    });
+
+    const params = {
+      lat: this.state.latitude,
+      lng: this.state.longitude,
+      page: this.state.page,
+      pageSize: this.state.pageSize,
+      q: e.target.value,
+    };
+    this.debouncedGetRestaurants(params);
+  }
+
   render() {
+    const items = this.state.items.map((item, index) => 
+      <li key={item.name + index}>
+        <div>{item.name}</div>
+        <div>{item.dist}</div>
+        <div>
+          <span>{item.block ? `${item.block} ` : ''}</span>
+          <span>{item.street}</span>
+          <span>{item.unit ? ` #${item.unit}` : ''}</span>
+        </div>
+      </li>
+    );
+
     return (
-      <form>
-        <label htmlFor="name-query">query</label>
-        <input 
-          id="name-query"
-          type="text"
-          placeholder="search"
-          value={this.state.query}
-          onChange={this.handleQueryInputChange}
-        />
-      </form>
+      <div className="container">
+        <form>
+          <label htmlFor="search-query">query</label>
+          <input 
+            id="search-query"
+            type="text"
+            placeholder="search"
+            value={this.state.query}
+            onChange={this.handleQueryInputChange}
+          />
+        </form>
+
+        <ul>
+          {items}
+        </ul>
+      </div>
     );
   }
 }
