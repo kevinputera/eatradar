0d056010bc15c63f66f91b5efa46f12a3cfa8a4f
Justin <--global>
2019-06-16 23:33:58 +0800

    removed test

--- server/test.js
+++
@@ -1,9 +0,0 @@
-const restaurantService = require('./service/restaurantService')
-const googlePlacesApiService = require('./service/googlePlacesApiService')
-const reviewService = require('./service/reviewService')
-const detailService = require('./service/detailService')
-
-//restaurantService.getClosestRestaurants({longitude: 0, latitude: 0, page: 1, pageSize:10}).then(console.log);
-
-detailService.getDetails(1)
-  .then(console.log);
