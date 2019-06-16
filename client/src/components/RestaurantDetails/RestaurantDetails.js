f5635522025315435b31bdd25fe1f69f38122304
Kevin Putera <kevintiosanna@gmail.com>
2019-06-16 23:07:20 +0800

    add bootstrap

---
+++ client/src/components/RestaurantDetails/RestaurantDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RestaurantDetails.css';
+
+class RestaurantDetails extends React.Component {
+  render() {
+    return (
+      <p className="lead text-muted">
+        Restaurant details here.
+      </p>
+    );
+  }
+}
+
+export default RestaurantDetails;
