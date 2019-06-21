5379bb303b784ca16ef91b673b400839e419b892
Kevin Putera <kevintiosanna@gmail.com>
2019-06-21 21:32:50 +0800

    Load blueprint

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Blueprint
+import 'normalize.css/normalize.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
