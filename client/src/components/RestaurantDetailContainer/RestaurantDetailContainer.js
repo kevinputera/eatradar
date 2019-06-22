31cc76716363078c296cecf034350021e39c98f1
Kevin Putera <kevintiosanna@gmail.com>
2019-06-22 14:41:31 +0800

    Add close button

--- client/src/components/RestaurantDetailContainer/RestaurantDetailContainer.js
+++ client/src/components/RestaurantDetailContainer/RestaurantDetailContainer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Button } from '@blueprintjs/core';
+
 import './RestaurantDetailContainer.css';
 
 class RestaurantDetailContainer extends React.Component {
   render() {
     return (
       <div className="restaurant-detail-container">
-        
+        <div className="close-button-wrapper">
+          <Button
+            large
+            minimal
+            icon="cross"
+            style={{ borderRadius: '20px' }}
+          />
+        </div>
       </div>
     );
   }
