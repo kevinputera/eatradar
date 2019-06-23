0b51a5bc0c717589cf1e4afd8986b5d6c1fc0e63
Kevin Putera <kevintiosanna@gmail.com>
2019-06-23 18:22:08 +0800

    add timeout

--- server/config/googlePlacesConfig.js
+++ server/config/googlePlacesConfig.js
@@ -2,7 +2,8 @@ const mapsFuncs = require('@google/maps');
 
 const placesClient = mapsFuncs.createClient({
   key: process.env.GOOGLE_MAPS_API_KEY_MASTER,
-  Promise: Promise
+  timeout: 2000,
+  Promise: Promise,
 });
 
 module.exports = { placesClient };
