bc7a87d688f390b0d5d4a457d6cb7808ba3b0f8d
Kevin Putera <kevintiosanna@gmail.com>
2019-06-29 10:21:10 +0800

    Add params object

--- server/routes/blogPostRoute.js
+++ server/routes/blogPostRoute.js
@@ -7,15 +7,13 @@ const response = require('../utils/response');
  * Get all blog posts of a restaurant based on id.
  */
 router.get('/:id', async (req, res) => {
-  let id;
-  try {
-    id = parseInt(req.params.id);
-  } catch (e) {
-    response.sendBadRequest(res, 'Id must be of type number');
-    return;
-  }
+  const params = {
+    id: req.params.id,
+    page: req.query.p || 1,
+    pageSize: req.query.ps || 5,
+  };
 
-  if (!id || id < 0) {
+  if (!params.id || params.id < 0) {
     response.sendBadRequest(
       res,
       'A correct id must be included in the request URI'
@@ -23,8 +21,21 @@ router.get('/:id', async (req, res) => {
     return;
   }
 
+  if (params.page <= 0) {
+    response.sendBadRequest(
+      res,
+      'Page query parameter[p] must be greater than zero'
+    );
+    return;
+  }
+
+  if (params.pageSize < 5) {
+    response.sendBadRequest(res, 'Page size must be greater than 5');
+    return;
+  }
+
   try {
-    const result = await blogPostService.getBlogPosts(id);
+    const result = await blogPostService.getBlogPosts(params);
     response.sendOk(res, result);
   } catch (e) {
     response.sendInternalError(res, e.message);
