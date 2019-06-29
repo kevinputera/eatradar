1ba28352e2f48e7b721a1e96077c4740b7043eba
Kevin Putera <kevintiosanna@gmail.com>
2019-06-29 16:42:51 +0800

    Display blogposts

--- client/src/components/RestaurantDetailBlogpost/RestaurantDetailBlogpost.js
+++ client/src/components/RestaurantDetailBlogpost/RestaurantDetailBlogpost.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Button } from '@blueprintjs/core';
+import { Card, Elevation } from '@blueprintjs/core';
+import NavigationButtons from '../NavigationButtons/NavigationButtons';
 
 import './RestaurantDetailBlogpost.css';
 
+function Blogpost(props) {
+  return (
+    <div className="blogpost">
+      <Card elevation={Elevation.ONE}>
+        <div className="blogpost-content blogpost-title">
+          <strong>{props.title}</strong>
+        </div>
+        <div className="blogpost-content blogpost-body">
+          {props.body}
+        </div>
+        <div className="blogpost-content blogpost-link">
+          {props.link}
+        </div>
+      </Card>
+    </div>
+  )
+}
+
 function RestaurantDetailBlogpost(props) {
+  const blogposts = props.blogPosts.map(blogPost => 
+    <Blogpost 
+      title={blogPost.title}
+      body={blogPost.post}
+      link={blogPost.link}
+    />
+  );
+
   return (
     <div className="restaurant-detail-blogpost">
       {props.blogPostsCount <= 0 ? (
@@ -15,7 +42,15 @@ function RestaurantDetailBlogpost(props) {
           <div className="detail-header">
             Blog posts you might find interesting
           </div>
-          <Button></Button> 
+          <div className="blogposts-wrapper">
+            {blogposts}
+          </div>
+          <div className="detail-navigation">
+            <NavigationButtons 
+              handlePagePrev={true} 
+              handlePageNext={true}
+            />
+          </div>
         </>
       )}
     </div>
--- client/src/components/RestaurantListNavigation/RestaurantListNavigation.js
+++ client/src/components/RestaurantListNavigation/RestaurantListNavigation.js
@@ -19,16 +19,6 @@ function RestaurantListNavigation(props) {
   const pageSizes = [10, 20, 30];
   return (
     <div className="restaurant-list-navigation">
-      <div className="navigation-button">
-        <ButtonGroup>
-          <Tooltip position="top" content="Previous page">
-            <Button icon="chevron-left" onClick={props.handlePagePrev} />
-          </Tooltip>
-          <Tooltip position="top" content="Next page">
-            <Button icon="chevron-right" onClick={props.handlePageNext} />
-          </Tooltip>
-        </ButtonGroup>
-      </div>
       <div className="navigation-selector">
         <Select
           items={pageSizes}
@@ -41,6 +31,16 @@ function RestaurantListNavigation(props) {
             <Button text={props.pageSize} rightIcon="double-caret-vertical" />
           </Tooltip>
         </Select>
+      </div>
+      <div className="navigation-button">
+        <ButtonGroup>
+          <Tooltip position="top" content="Previous page">
+            <Button icon="chevron-left" onClick={props.handlePagePrev} />
+          </Tooltip>
+          <Tooltip position="top" content="Next page">
+            <Button icon="chevron-right" onClick={props.handlePageNext} />
+          </Tooltip>
+        </ButtonGroup>
       </div>
     </div>
   );
