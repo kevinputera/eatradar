44df77dfe9ded6eb7cd6d3ef99d7f53b8cb790a4
Kevin Putera <kevintiosanna@gmail.com>
2019-06-29 10:00:09 +0800

    Add multi-search blogpost query

--- server/service/blogPostService.js
+++ server/service/blogPostService.js
@@ -4,24 +4,31 @@ const restaurantService = require('./restaurantService');
 /**
  * Get all blog posts for a restaurant
  *
- * @param {number} id
+ * @param {Object} params
+ * @param {number} params.id
+ * @param {number} params.page
+ * @param {number} params.pageSize
  * @return {Promise<Object>}
  */
-exports.getAllBlogPosts = async id => {
-  const restaurant = await restaurantService.getRestaurant(id).name;
+exports.getAllBlogPosts = async params => {
+  const name = await restaurantService.getRestaurant(params.id).name;
   const res = await esClient.search({
     index: 'blogpost',
     body: {
       query: {
-        match: {
-          post: {
-            query: restaurant,
-            fuzziness: 1,
-          },
+        multi_match: {
+          query: name,
+          fuzziness: 1,
+          fields: ['title^2', 'post'],
         },
       },
     },
+    from: params.page - 1,
+    size: params.pageSize,
   });
 
-  console.log(res);
+  return {
+    count: res.body.hits.total.value,
+    hits: res.body.hits.hits,
+  };
 };
