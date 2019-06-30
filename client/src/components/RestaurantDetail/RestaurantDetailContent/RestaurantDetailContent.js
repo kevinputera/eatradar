6b1857594398902bc2f0aec06210addf48f917dc
Kevin Putera <kevintiosanna@gmail.com>
2019-06-30 10:33:23 +0800

    Refactor RestaurantDetailContent

--- client/src/components/RestaurantDetail/RestaurantDetailContent/RestaurantDetailContent.js
+++ client/src/components/RestaurantDetail/RestaurantDetailContent/RestaurantDetailContent.js
@@ -6,22 +6,39 @@ import './RestaurantDetailContent.css';
 import poweredByGoogle from './powered_by_google_on_white_hdpi.png';
 
 function RestaurantDetailContent(props) {
-  let phone;
-  let website;
-  let hours;
-  if (props.details) {
-    phone = props.details.phone_number;
-    website = props.details.website;
-    hours = props.details.opening_hours;
-    // photos = props.details.photos;
-  }
+  const empty =
+    !props.details.phone_number &&
+    !props.details.website &&
+    !props.details.opening_hours;
 
-  let empty = false;
-  if (!phone && !website && !hours) {
-    empty = true;
-  }
+  const phone = props.details.phone_number && (
+    <FieldContent title="Phone number" body={props.details.phone_number} />
+  );
+
+  const website = props.details.website && (
+    <FieldContent
+      title="Website"
+      body={
+        <a
+          href={props.details.website}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {props.details.website}
+        </a>
+      }
+    />
+  );
 
   const day = new Date().getDay();
+  const hours = props.details.opening_hours && (
+    <FieldContent
+      title="Opening hours"
+      body={`${props.details.opening_hours.periods.get(day).open.time} - ${
+        props.details.opening_hours.periods.get(day).close.time
+      }`}
+    />
+  );
 
   return (
     <div className="restaurant-detail-content">
@@ -32,26 +49,12 @@ function RestaurantDetailContent(props) {
       ) : (
         <>
           <div className="detail-header">Details</div>
-          {phone && <FieldContent title="Phone number" body={phone} />}
-          {website && (
-            <FieldContent
-              title="Website"
-              body={
-                <a href={website} target="_blank" rel="noopener noreferrer">
-                  {website}
-                </a>
-              }
-            />
-          )}
-          {hours && (
-            <FieldContent
-              title="Opening hours"
-              body={`${hours.periods.get(day).open.time} - ${
-                hours.periods.get(day).close.time
-              }`}
-            />
-          )}
-          <div className="detail-photos"></div>
+          <div className="contents-wrapper">
+            {phone}
+            {website}
+            {hours}
+            <div className="detail-photos"></div>
+          </div>
           <div className="attribution-wrapper">
             <img
               className="attribution"
