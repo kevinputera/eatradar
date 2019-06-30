01f9ecb25db98a9f6a71320a7f0930c1d2733228
Kevin Putera <kevintiosanna@gmail.com>
2019-06-30 09:56:14 +0800

    Generalize ExtendableContent component

--- client/src/components/shared/ExtendableContent/ExtendableContent.js
+++ client/src/components/shared/ExtendableContent/ExtendableContent.js
@@ -7,31 +7,46 @@ import './ExtendableContent.css';
 function ExtendableContent(props) {
   const [isExtended, setExtended] = useState(false);
 
+  const title = props.title && (
+    <>
+      <strong>{props.title}</strong>
+      <Divider />
+    </>
+  );
+
+  const extendBtn = props.extendable && (
+    <span className="toggle-extended" onClick={() => setExtended(!isExtended)}>
+      {isExtended ? 'Hide' : 'Extend'}
+    </span>
+  );
+
+  const link = props.link && isExtended && (
+    <a
+      className="extendable-content-link"
+      href={props.link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Read more
+    </a>
+  );
+
+  const content = props.extendable
+    ? isExtended
+      ? props.body
+      : getSummary(props.body, 20)
+    : props.body;
+
   return (
     <div className="extendable-content">
       <Card elevation={Elevation.ONE}>
         <div className="extendable-content-c extendable-content-title">
-          <strong>{props.title}</strong>
+          {title}
         </div>
-        <Divider />
         <div className="extendable-content-c extendable-content-body">
-          {isExtended ? props.body : getSummary(props.body, 20)}
-          <span
-            className="toggle-extended"
-            onClick={() => setExtended(!isExtended)}
-          >
-            {isExtended ? 'Hide' : 'Extend'}
-          </span>
-          {isExtended && (
-            <a
-              className="extendable-content-link"
-              href={props.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read more
-            </a>
-          )}
+          {content}
+          {extendBtn}
+          {link}
         </div>
       </Card>
     </div>
