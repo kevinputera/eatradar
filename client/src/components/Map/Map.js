d189b5f6276df9a3c2b6f3a85c50db1c054db7f8
Kevin Putera <kevintiosanna@gmail.com>
2019-07-07 15:38:58 +0800

    Use hooks to display restaurants on a map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useMap } from '../../hooks/mapHooks';
+import { useMap, useFeatureCollectionMarkers } from '../../hooks/mapHooks';
 
 import './Map.css';
 
 function Map(props) {
   const secret = process.env.REACT_APP_MAPBOX_ACCESSTOKEN;
-  const params = {
+
+  const map = useMap(secret, {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [103.9089594, 1.347636],
     zoom: 10.5,
-  };
+  });
 
-  const map = useMap(secret, params);
+  useFeatureCollectionMarkers(secret, map, '/restaurants');
 
   return <div id="map" />;
 }
