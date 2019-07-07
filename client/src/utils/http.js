e4fa9a2549ba226f4564a14e8da3c2109fedc734
Kevin Putera <kevintiosanna@gmail.com>
2019-07-07 16:04:03 +0800

    Fix null pointer exception bug

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,7 +2,7 @@
  * Do a GET HTTP request to url.
  *
  * @param {string} url The URL endpoint
- * @param {Object} params The parameters used in the HTTP request
+ * @param {Object} [params] The parameters used in the HTTP request
  * @param {Object} [params.qs] Query string to be appended to request URL
  * @param {Object} [params.headers] Headers to be used in HTTP request
  * @throws {Error} If response status is not in the 200-299 range
@@ -11,7 +11,7 @@
 export const get = async (url, params) => {
   const endpoint = new URL(url);
 
-  if (params.qs) {
+  if (params && params.qs) {
     Object.keys(params.qs).forEach(key =>
       endpoint.searchParams.append(key, params.qs[key])
     );
@@ -19,7 +19,7 @@ export const get = async (url, params) => {
 
   const config = {
     method: 'GET',
-    headers: params.headers || { Accept: 'application/json' },
+    headers: (params && params.headers) || { Accept: 'application/json' },
   };
   const res = await fetch(endpoint, config);
   const json = await res.json();
