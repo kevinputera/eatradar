1e5f7512891e55c6c24c6febb67ed7544d8d68d8
Kevin Putera <kevintiosanna@gmail.com>
2019-07-12 23:03:48 +0800

    Add get single restaurant route, split restaurantRoute and restaurantLocationRoute

--- server/routes/restaurantRoute.js
+++ server/routes/restaurantRoute.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const restaurantRoute = express.Router();
-const restaurantLocationRoute = express.Router();
+const router = express.Router();
 const restaurantService = require('../service/restaurantService');
 const response = require('../utils/response');
 
 /**
- * Get all restaurants in Singapore in the form of GeoJSON data
- */
-restaurantLocationRoute.get('/', async (req, res) => {
-  try {
-    const result = await restaurantService.getRestaurantLocationsGeoJSON(
-      req.query.q
-    );
-    response.sendOk(res, result);
-  } catch (e) {
-    response.sendInternalError(res, e.message);
-  }
-});
-
-/**
  * Get closest restaurants based on location.
  */
-restaurantRoute.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
   const params = {
     longitude: req.query.lng,
     latitude: req.query.lat,
@@ -59,7 +44,32 @@ restaurantRoute.get('/', async (req, res) => {
   }
 });
 
-module.exports = {
-  restaurantLocationRoute,
-  restaurantRoute,
-};
+/**
+ * Get a single restaurant by its id.
+ */
+router.get('/:id', async (req, res) => {
+  let id;
+  try {
+    id = parseInt(req.params.id);
+  } catch (e) {
+    response.sendBadRequest(res, 'Id must be of type number');
+    return;
+  }
+
+  if (!id || id < 0) {
+    response.sendBadRequest(
+      res,
+      'A correct id must be included in the request URI'
+    );
+    return;
+  }
+
+  try {
+    const result = await restaurantService.getRestaurant(id);
+    response.sendOk(res, result);
+  } catch (e) {
+    response.sendInternalError(res, e.message);
+  }
+});
+
+module.exports = router;
