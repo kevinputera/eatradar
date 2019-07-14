b55c864d78e57310a9a7058fb33a1c9879b49775
Kevin Putera <kevin.putera@visenze.com>
2019-07-14 11:21:55 +0800

    Remove refresh button temporarily

--- client/src/components/SearchFilter/SearchFilter.js
+++ client/src/components/SearchFilter/SearchFilter.js
@@ -6,29 +6,28 @@ import './SearchFilter.css';
 function SearchFilter(props) {
   return (
     <div className="search-filter">
-      <div className="search-filter-input-wrapper">
-        <InputGroup
-          large
-          leftIcon="search"
-          placeholder="Search restaurants"
-          value={props.query}
-          onChange={props.handleQueryInputChange}
-          style={{ borderRadius: '10px' }}
-        />
-      </div>
-      <div className="refresh-button-wrapper">
-        <Tooltip position="bottom" content="Update your location">
-          <Button
-            large
-            minimal
-            icon="refresh"
-            onClick={props.handleRefreshButtonClick}
-            style={{ borderRadius: '20px' }}
-          />
-        </Tooltip>
-      </div>
+      <InputGroup
+        large
+        leftIcon="search"
+        placeholder="Search restaurants"
+        value={props.query}
+        onChange={props.handleQueryInputChange}
+        style={{ borderRadius: '10px' }}
+      />
     </div>
   );
 }
+
+// <div className="refresh-button-wrapper">
+//   <Tooltip position="bottom" content="Update your location">
+//     <Button
+//       large
+//       minimal
+//       icon="refresh"
+//       onClick={props.handleRefreshButtonClick}
+//       style={{ borderRadius: '20px' }}
+//     />
+//   </Tooltip>
+// </div>
 
 export default SearchFilter;
