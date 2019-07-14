5813ddefb70461a73267f8ee02941004d638e5d4
Kevin Putera <kevintiosanna@gmail.com>
2019-07-14 22:10:17 +0800

    Remove blog post pagination, prepare for creating carousel

--- server/routes/blogPostRoute.js
+++ server/routes/blogPostRoute.js
@@ -13,51 +13,6 @@ router.get('/:id', async (req, res) => {
     return;
   }
 
-  const params = {
-    id,
-    page: parseInt(req.query.p) || 1,
-    pageSize: parseInt(req.query.ps) || 3,
-  };
-
-  if (params.id < 0) {
-    response.sendBadRequest(
-      res,
-      'A correct id must be included in the request URI'
-    );
-    return;
-  }
-
-  if (params.page <= 0) {
-    response.sendBadRequest(
-      res,
-      'Page query parameter[p] must be greater than zero'
-    );
-    return;
-  }
-
-  if (params.pageSize < 3) {
-    response.sendBadRequest(res, 'Page size must be greater than 3');
-    return;
-  }
-
-  try {
-    const result = await blogPostService.getBlogPosts(params);
-    response.sendOk(res, result);
-  } catch (e) {
-    response.sendInternalError(res, e.message);
-  }
-});
-
-/**
- * Get the number of blog posts for a restaurant id
- */
-router.get('/:id/count', async (req, res) => {
-  const id = parseInt(req.params.id);
-  if (!Number.isInteger(id)) {
-    response.sendBadRequest(res, 'Id must be of type number');
-    return;
-  }
-
   if (id < 0) {
     response.sendBadRequest(
       res,
@@ -67,8 +22,8 @@ router.get('/:id/count', async (req, res) => {
   }
 
   try {
-    const result = await blogPostService.getBlogPostsCount(id);
-    response.sendOk(res, { count: result });
+    const result = await blogPostService.getBlogPosts(id);
+    response.sendOk(res, result);
   } catch (e) {
     response.sendInternalError(res, e.message);
   }
