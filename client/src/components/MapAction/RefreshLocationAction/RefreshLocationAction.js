05339b9ecc78c379de5b66636bb44a1d33a5e0d1
Kevin Putera <kevintiosanna@gmail.com>
2019-07-20 14:57:13 +0800

    Add styles to actions

--- client/src/components/MapAction/RefreshLocationAction/RefreshLocationAction.js
+++ client/src/components/MapAction/RefreshLocationAction/RefreshLocationAction.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Button } from '@blueprintjs/core';
 
 import './RefreshLocationAction.css';
 
 function RefreshLocationAction(props) {
-  return <button onClick={props.refreshLocation}></button>;
+  return (
+    <div className="refresh-location-action">
+      <Button
+        large
+        icon="locate"
+        onClick={props.refreshLocation}
+        style={{ borderRadius: '20px' }}
+      />
+    </div>
+  );
 }
 
 export default RefreshLocationAction;
--- client/src/components/MapAction/ZoomAction/ZoomAction.js
+++ client/src/components/MapAction/ZoomAction/ZoomAction.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Button } from '@blueprintjs/core';
 
 import './ZoomAction.css';
 
 function ZoomAction(props) {
   return (
     <div className="zoom-action">
-      <button onClick={props.handleZoomIn}>+</button>
-      <button onClick={props.handleZoomOut}>-</button>
+      <div className="zoom-control">
+        <Button
+          large
+          icon="small-plus"
+          onClick={props.handleZoomIn}
+          style={{ borderRadius: '20px' }}
+        />
+      </div>
+      <div className="zoom-control">
+        <Button
+          large
+          icon="small-minus"
+          onClick={props.handleZoomOut}
+          style={{ borderRadius: '20px' }}
+        />
+      </div>
     </div>
   );
 }
