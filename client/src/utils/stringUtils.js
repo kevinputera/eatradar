b391fee2db2b6744c41c9a0a15040d531bfbbd77
Kevin Putera <kevintiosanna@gmail.com>
2019-07-21 22:51:44 +0800

    Add more string utils

--- client/src/utils/stringUtils.js
+++ client/src/utils/stringUtils.js
@@ -17,3 +17,23 @@ export const getDistanceString = dist => {
   }
   return `${Math.round(round / 1000)}km`;
 };
+
+export const capitalize = string => {
+  if (!isString(string)) {
+    throw new Error('Input is not a string');
+  }
+
+  const tokens = string.split(' ');
+  return tokens
+    .map(
+      token => token.charAt(0).toUpperCase() + token.toLowerCase().slice(1)
+    )
+    .join(' ');
+};
+
+export const isString = string => {
+  if (typeof string !== 'string') {
+    return false;
+  }
+  return true;
+};
