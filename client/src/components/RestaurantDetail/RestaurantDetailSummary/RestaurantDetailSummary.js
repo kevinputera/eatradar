9bfd4bc85bd396911329b6f370304de3363e212d
Kevin Putera <kevintiosanna@gmail.com>
2019-07-21 22:52:39 +0800

    Revamp restaurant detail summary ui

--- client/src/components/RestaurantDetail/RestaurantDetailSummary/RestaurantDetailSummary.js
+++ client/src/components/RestaurantDetail/RestaurantDetailSummary/RestaurantDetailSummary.js
@@ -1,29 +1,39 @@
 import React from 'react';
+import GoogleRating from '../GoogleRating/GoogleRating';
 
 import './RestaurantDetailSummary.css';
 
 function RestaurantDetailSummary(props) {
+  const noRatings = !props.ratings.google && !props.ratings.yelp;
   return (
     <div className="restaurant-detail-summary">
-      {props.restaurant && (
-        <>
-          <div className="restaurant-detail-name">
-            {props.restaurant.name}
-          </div>
-          <div className="restaurant-detail-address">
-            <span>{props.restaurant.block} </span>
-            <span>{props.restaurant.street}</span>
-          </div>
-          <div className="restaurant-detail-address">
-            {props.restaurant.level && props.restaurant.unit ? (
-              <>
-                <span>#{props.restaurant.level}-</span>
-                <span>{props.restaurant.unit}, </span>
-              </>
-            ) : null}
-            <span>{props.restaurant.postcode}</span>
-          </div>
-        </>
+      {props.isRestaurantLoading ? (
+        <div className="restaurant-detail-name-loading">
+          Loading name...
+        </div>
+      ) : (
+        <div className="restaurant-detail-name">
+          {props.restaurant.name}
+        </div>
+      )}
+      {props.isReviewsLoading ? (
+        <div className="restaurant-detail-ratings-loading">
+          Loading reviews...
+        </div>
+      ) : (
+        <div className="restaurant-detail-ratings">
+          {props.ratings.google && (
+            <div className="restaurant-google-rating-wrapper">
+              <GoogleRating rating={props.ratings.google} />
+            </div>
+          )}
+          {props.ratings.yelp && (
+            <div className="restaurant-yelp-rating-wrapper"></div>
+          )}
+          {noRatings && (
+            <div className="ratings-not-found">No ratings</div>
+          )}
+        </div>
       )}
     </div>
   );
