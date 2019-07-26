d64eb30fcead762bc992e006caf808029c22c89c
Kevin Putera <kevintiosanna@gmail.com>
2019-07-26 21:47:43 +0800

    Refactor response.test.js

--- server/__tests__/utils/response.test.js
+++ server/__tests__/utils/response.test.js
@@ -12,15 +12,12 @@ beforeEach(() => {
 
 test('sendOk must return 200 status and supplied body', () => {
   const body = { payload: 5 };
+  const response = { status: 200, message: '', data: body };
+
   sendOk(res, body);
 
   expect(res.status).toHaveBeenCalledTimes(1);
   expect(res.status).toHaveBeenCalledWith(200);
-
   expect(res.send).toHaveBeenCalledTimes(1);
-  expect(res.send).toHaveBeenCalledWith({
-    status: 200,
-    message: '',
-    data: body,
-  });
+  expect(res.send).toHaveBeenCalledWith(response);
 });
