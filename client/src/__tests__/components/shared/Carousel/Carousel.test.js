eadf17c05e7955a23269b607550497147579c2d1
Kevin Putera <kevintiosanna@gmail.com>
2019-07-28 21:57:24 +0800

    Update tests for Carousel

--- client/src/__tests__/components/shared/Carousel/Carousel.test.js
+++ client/src/__tests__/components/shared/Carousel/Carousel.test.js
@@ -5,7 +5,7 @@ import Carousel from '../../../../components/shared/Carousel/Carousel';
 
 describe('tests for Carousel component', () => {
   test('render must match snapshot', () => {
-    const contents = [{ post: 'A post' }];
+    const contents = [{ post: 'Post 1' }, { post: 'Post 2' }, { post: 'Post 3' }];
     const tree = create(<Carousel contents={contents} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
